dbb9b5b3a0999e5a6afbead6ebb49b595dbcf56c
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2021-12-18 11:29:47 -0500

    Feature/Hasta clase 17
    Creacion de proyecto Angular y configuracion de rutas, guardas y lazy loading

--- 03-angular/ejemplo/src/app/app-routing.module.ts
+++ 03-angular/ejemplo/src/app/app-routing.module.ts
@@ -1,11 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
+import {RutaForbbidenComponent} from "./rutas/ruta-forbbiden/ruta-forbbiden.component";
+import {RutaNotFoundComponent} from "./rutas/ruta-not-found/ruta-not-found.component";
+import {RutaInicioComponent} from "./rutas/ruta-inicio/ruta-inicio.component";
+import {RutaAppComponent} from "./rutas/ruta-app/ruta-app.component";
+import {RutaUsuarioComponent} from "./rutas/ruta-usuario/ruta-usuario.component";
+import {RutaPostComponent} from "./rutas/ruta-post/ruta-post.component";
+import {EstaLogueadoGuard} from "./servicios/auth/esta-logueado.guard";
+import {EsAdministradorGuard} from "./servicios/auth/es-administrador.guard";
 
 const routes: Routes = [
   {
+    path: 'inicio',
+    canActivate:[EstaLogueadoGuard],
+    component: RutaInicioComponent
+  },
+  {
+    path:'app',
+    component: RutaAppComponent,
+    children:[
+      {
+        path:'usuario',
+        component: RutaUsuarioComponent
+      },
+      {
+        path: 'post',
+        component: RutaPostComponent,
+        canActivate: [EsAdministradorGuard]
+      }
+    ]
+  },
+  {
     path: 'login',
     component: RutaLoginComponent
+  },
+  {
+    path: 'forbidden',
+    component: RutaForbbidenComponent
+  },
+  {
+    path: 'not-found',
+    component: RutaNotFoundComponent
+  },
+  {
+    path:'',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'lazy-inventario',
+    loadChildren: () =>  import("./modulos/modulo-inventario/modulo-inventario.module") //import('./modulos/modulo-inventario/modulo-inventario.module')
+      .then(m => m.ModuloInventarioModule)
+  }
+  ,
+  {//para cuando va a una pagina que no existe
+    path:'**',
+    component: RutaNotFoundComponent
   }
 ];
 
