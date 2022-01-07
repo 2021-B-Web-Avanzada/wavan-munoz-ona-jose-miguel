c7f6f8f71a0b15a3ff70a6907f7fae10f6ea228f
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-01-07 13:07:42 -0500

    Update: tweet-bar.component
    Adicion de botones de accion y de span para texto

--- Examen1B/twitter-clone/src/app/components/ui-elements/ui-elements.module.ts
+++ Examen1B/twitter-clone/src/app/components/ui-elements/ui-elements.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 import { ProfileInfoBtnComponent } from './profile-info-btn/profile-info-btn.component';
+import { TweetBarComponent } from './tweet-bar/tweet-bar.component';
+import { IconBtnComponent } from './icon-btn/icon-btn.component';
 
 
 
 @NgModule({
   declarations: [
     ProfilePictureComponent,
-    ProfileInfoBtnComponent
+    ProfileInfoBtnComponent,
+    TweetBarComponent,
+    IconBtnComponent
   ],
   exports: [
     ProfilePictureComponent,
-    ProfileInfoBtnComponent
+    ProfileInfoBtnComponent,
+    TweetBarComponent,
+    IconBtnComponent
   ],
   imports: [
     CommonModule
