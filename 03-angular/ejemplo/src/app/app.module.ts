6bf07bea241ce189e972a61261f307f9e6ce0a1a
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-01-12 08:56:58 -0500

    Clase 23

--- 03-angular/ejemplo/src/app/app.module.ts
+++ 03-angular/ejemplo/src/app/app.module.ts
@@ -17,6 +17,8 @@ import {RutaReporteComponent} from "./modulos/modulo-inventario/rutas/ruta-repor
 import {RouterModule} from "@angular/router";
 import {BannerImagenesComponent} from "./Componentes/banner-imagenes/banner-imagenes/banner-imagenes.component";
 import {BannerImagenesModule} from "./Componentes/banner-imagenes/banner-imagenes.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   //Componentes
@@ -35,7 +37,9 @@ import {BannerImagenesModule} from "./Componentes/banner-imagenes/banner-imagene
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    BannerImagenesModule //esto en el app.module
+    BannerImagenesModule, //esto en el app.module
+    HttpClientModule,
+    FormsModule
   ],
   //servicios
   providers: [
