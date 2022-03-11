df644d6219fb185315091207c90ddd1e30203ea5
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-03-11 11:37:22 -0500

    Add: Proyecto segundo bimestre

---
+++ Proyecto/Segundo Bimestre/boxed-client/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  urlBoxed:'http://localhost:3000',
+  firebase: {
+    projectId: 'wavan-munoz-villegas-proyecto',
+    appId: '1:816645783023:web:649b716b921d9694bd4a1a',
+    storageBucket: 'wavan-munoz-villegas-proyecto.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBtms4ANSRp6ik4HwDhfaVIVyPZaAxBdSQ',
+    authDomain: 'wavan-munoz-villegas-proyecto.firebaseapp.com',
+    messagingSenderId: '816645783023',
+    measurementId: 'G-7X50F1P1N7',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
